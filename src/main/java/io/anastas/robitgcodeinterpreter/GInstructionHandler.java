97cacae478f2afffbe9db232ef52862718b68095
KingRandomGuy <jimtjames@gmail.com>
2016-05-30 15:52:29 -0500

    Start writing G1 handler

--- src/main/java/io/anastas/robitgcodeinterpreter/GInstructionHandler.java
+++ src/main/java/io/anastas/robitgcodeinterpreter/GInstructionHandler.java
@@ -29,23 +29,21 @@ public class GInstructionHandler {
 		TouchSensor yTouch = new TouchSensor();
 		brickPi.setSensor(xTouch, SensorPort.S1);
 		brickPi.setSensor(yTouch, SensorPort.S2);
-		while (!xTouch.isSet()) {
+		/**while (!xTouch.isSet()) {
 			xAxis.rotate(-0.01, 5);
 		}
 		while (!yTouch.isSet()) {
 			yAxis.rotate(-0.01, 5);
-		}
-		try {
-			brickPi = new BrickPi();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		}**/
 		double xyRotationToMM = 1/127.2;
 		// Loop through each element of the list and execute
 		for (int i=0; i<instructionList.size(); i++) {
 			GInstruction curInstruction = instructionList.get(i);
 			System.out.println("Executing Instruction type: " + curInstruction.type);
-			System.out.println("Previous type was: " + instructionList.get(i-1).type);
+			//System.out.println("Previous type was: " + instructionList.get(i-1).type);
+			if (curInstruction.type.equals("G1")) {
+				//xAxis.rotate((curInstruction.xcoord-prevInstruction.xcoord)*xyRotationToMM, 5);
+			}
 		}
 	}
 }
